0c78ce01cb31fbb95fdb2c35f9785155ed0729ac
jesaerys <jacob.simones@gmail.com>
2014-05-23 11:43:27 -0500

    Updated __init__.py

---
+++ sfhmaps/__init__.py
@@ -0,0 +1,23 @@
+"""
+=======
+sfhmaps
+=======
+
+Utilities for the sfh_maps project and paper.
+
+(Expanded package description)
+
+(List of classes and functions with one-line descriptions)
+
+(Optional Notes section)
+
+
+###
+- analysis routines
+- plotting routines
+- tools for uniform formatting in figures
+
+"""
+#from researchtools.sfhmaps.subpackage import thing
+from . import config
+from . import util
